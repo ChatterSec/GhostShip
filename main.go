6ec4417600cedb22bfbd32527b327085f576f155
Reece Harris <reeceharris@email.com>
2024-05-28 14:17:11 +0100

    Refactor main.go to improve terminal input handling

--- main.go
+++ main.go
@@ -42,9 +42,70 @@ func main() {
 		exec.Command("stty", "-F", "/dev/tty", "-echo").Run()              // do not display entered characters on the screen
 	}
 
+	hovering := 0
+	options := []string{"module", "booty", "help", "report", "exit"}
+	colors := make([]string, len(options))
+	cursor := make([]string, len(options))
+	first_loop := true
+
 	var b []byte = make([]byte, 1)
 	for {
-		os.Stdin.Read(b)
-		fmt.Println(b)
+		if !first_loop {
+			os.Stdin.Read(b)
+		}
+
+		// Up
+		if b[0] == 107 {
+			if hovering == 0 {
+				hovering = len(options) - 1
+			} else {
+				hovering -= 1
+			}
+		}
+
+		// Down
+		if b[0] == 106 {
+			if hovering == len(options)-1 {
+				hovering = 0
+			} else {
+				hovering += 1
+			}
+		}
+
+		if b[0] == 106 || b[0] == 107 || first_loop {
+			for i := range options {
+				if i == hovering {
+					colors[i] = "\033[32m"
+					cursor[i] = ">"
+				} else {
+					colors[i] = "\033[0m"
+					cursor[i] = "-"
+				}
+			}
+
+			if !first_loop {
+				fmt.Printf("\033[%dA", 16)
+				fmt.Print("\033[2K")
+			}
+
+			fmt.Println("\033[32m\033[1m" + `
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⡤⣤⣄⣾⣿⣿⣿⣿⣿⣿⣷⣠⣀⣄⡀` + "\033[0m\033[1m\t " + `GhostShip v0.1.0` + "\033[32m\033[1m" + `
+⠀⠀⠀⠀⠙⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣬⡿` + "\033[0m\t by: \033]8;;https://reeceharris.net\033\\notreeceharris\033]8;;\033\\\033[32m\033[1m" + `
+⠀⠀⠀⠀⠀⢀⣼⠟⢿⣿⣿⣿⣿⣿⣿⡿⠘⣷⣄
+⣰⠛⠛⣿⢠⣿⠋⠀⠀⢹⠻⣿⣿⡿⢻⠁⠀⠈⢿⣦` + "\033[0m\033[2m\t " + `A pirate-themed penetration testing framework,` + "\033[0m\033[32m\033[1m" + `
+⢈⣵⡾⠋⣿⣯⠀⠀⢀⣼⣷⣿⣿⣶⣷⡀⠀⠀⢸⣿⣀⣀` + "\033[0m\033[2m\t " + `built using reliable tools and custom scripts.` + "\033[0m\033[32m\033[1m" + `
+⢾⣿⣀⠀⠘⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⠿⣿⡁
+⠈⠙⠛⠿⠿⠿⢿⣿⡿⣿⣿⡿⢿⣿⣿⣿⣷⣄⠀⠘⢷⣆` + colors[0] + "\033[0m\t " + cursor[0] + ` modules` + "\033[32m\033[1m" + `
+⠀⠀⠀⠀⠀⢠⣿⠏⠀⣿⡏⠀⣼⣿⠛⢿⣿⣿⣆⠀⠀⣿⡇⡀` + colors[1] + "\033[0m " + cursor[1] + " booty \033[2m(captures)" + "\033[0m\033[32m\033[1m" + `⠀
+⠀⠀⠀⠀⢀⣾⡟⠀⠀⣿⣇⠀⢿⣿⡀⠈⣿⡌⠻⠷⠾⠿⣻⠁` + colors[2] + "\033[0m " + cursor[2] + " sos \033[2m(help)" + "\033[0m\033[32m\033[1m" + `⠀
+⠀⠀⣠⣶⠟⠫⣤⠀⠀⢸⣿⠀⣸⣿⢇⡤⢼⣧⠀⠀⠀⢀⣿` + colors[3] + "\033[0m\t " + cursor[3] + ` report issue` + "\033[32m\033[1m" + `⠀
+⠀⣾⡏⠀⡀⣠⡟⠀⠀⢀⣿⣾⠟⠁⣿⡄⠀⠻⣷⣤⣤⡾⠋` + colors[4] + "\033[0m\t " + cursor[4] + " disembark \033[2m(exit)" + "\033[0m\033[32m\033[1m" + `⠀
+⠀⠙⠷⠾⠁⠻⣧⣀⣤⣾⣿⠋⠀⠀⢸⣧⠀⠀⠀⠉⠁
+⠀⠀⠀⠀⠀⠀⠈⠉⠉⠹⣿⣄⠀⠀⣸⡿` + "\033[0m\033[2m\t " + `(k = up, j = down, enter = submit)` + "\033[0m\033[32m\033[1m" + `
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠿⠟⠛⠁` + "\033[0m")
+		}
+
+		first_loop = false
 	}
 }
