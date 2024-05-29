4931ba9c5c8e7a31eedf00d306926bb9bb5e1202
Reece Harris <reeceharris@email.com>
2024-05-29 10:15:49 +0100

    Refactor file structure and add links to external repositories
    This commit refactors the file structure by adding a new SOURCE.md file. It also adds links to three external repositories: SSH-Snake, WhatWeb, and Ghost. This change improves organization and provides easy access to the external repositories.

---
+++ SOURCE.md
@@ -0,0 +1,3 @@
+SSH-Snake: https://github.com/MegaManSec/SSH-Snake
+WhatWeb: https://github.com/urbanadventurer/WhatWeb
+Ghost: https://github.com/EntySec/Ghost
--- src/main.go
+++ src/main.go
@@ -55,12 +55,27 @@ func toolsMenu() {
 
 	var (
 		hovering          = 0
-		options           = []string{"Back", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test"}
+		options           = []string{"all", "osint", "exploitation", "post-exploitation", "reporting", "settings", "search", "back"}
 		colors            = make([]string, len(options))
 		cursor            = make([]string, len(options))
 		first_loop        = true
 		b          []byte = make([]byte, 1)
 	)
+
+	cutlass := []string{
+		"   \x1b[31m^\033[0m     ",
+		"  \x1b[31m|\033[0m.|    ",
+		"  |\x1b[31m|\033[0m|    ",
+		"  |\x1b[31m|\033[0m|    ",
+		"  \x1b[31m|\033[0m||    ",
+		"  \x1b[31m|\033[0m||    ",
+		"  |\x1b[31m|\033[0m|    ",
+		"\033[33m__\033[0m||\x1b[31m|\033[0m\033[33m_   \033[0m",
+		"\033[33m`----.`  \033[0m",
+		"\033[33m  ||  )) \033[0m",
+		"\033[33m  |'-',  \033[0m",
+		"\033[33m   '-'   \033[0m",
+	}
 
 	for {
 		if !first_loop {
@@ -68,7 +83,7 @@ func toolsMenu() {
 		}
 
 		// Handle "k" Key Press (aka up)
-		if b[0] == 107 {
+		if b[0] == 107 || b[0] == 75 {
 			if hovering == 0 {
 				hovering = len(options) - 1
 			} else {
@@ -77,7 +92,7 @@ func toolsMenu() {
 		}
 
 		// Handle "j" Key Press (aka down)
-		if b[0] == 106 {
+		if b[0] == 106 || b[0] == 74 {
 			if hovering == len(options)-1 {
 				hovering = 0
 			} else {
@@ -88,16 +103,16 @@ func toolsMenu() {
 		// Handle Space Bar Press
 		if b[0] == 32 {
 
-			if hovering == 0 { // Back
+			if hovering == 7 { // Back
 				clearTerminal()
 				mainMenu(true)
 			}
 		}
 
-		if b[0] == 106 || b[0] == 107 || first_loop {
+		if b[0] == 106 || b[0] == 74 || b[0] == 107 || b[0] == 75 || first_loop {
 			for i := range options {
 				if i == hovering {
-					colors[i] = "\033[32m"
+					colors[i] = "\033[33m"
 					cursor[i] = " >"
 				} else {
 					colors[i] = "\033[0m"
@@ -109,23 +124,42 @@ func toolsMenu() {
 			fmt.Print("\033[2K")
 
 			// Print Menu
+
 			fmt.Println(" ")
-			fmt.Println(colors[0] + cursor[0] + " Go Back  " + "\033[0m   | " + colors[15] + cursor[15] + " DetectDee")
-			fmt.Println(colors[1] + cursor[1] + " DetectDee" + "\033[0m   | " + colors[16] + cursor[16] + " DetectDee")
-			fmt.Println(colors[2] + cursor[2] + " DetectDee" + "\033[0m   | " + colors[17] + cursor[17] + " DetectDee")
-			fmt.Println(colors[3] + cursor[3] + " DetectDee" + "\033[0m   | " + colors[18] + cursor[18] + " DetectDee")
-			fmt.Println(colors[4] + cursor[4] + " DetectDee" + "\033[0m   | " + colors[19] + cursor[19] + " DetectDee")
-			fmt.Println(colors[5] + cursor[5] + " DetectDee" + "\033[0m   | " + colors[20] + cursor[20] + " DetectDee")
-			fmt.Println(colors[6] + cursor[6] + " DetectDee" + "\033[0m   | " + colors[21] + cursor[21] + " DetectDee")
-			fmt.Println(colors[7] + cursor[7] + " DetectDee" + "\033[0m   | " + colors[22] + cursor[22] + " DetectDee")
-			fmt.Println(colors[8] + cursor[8] + " DetectDee" + "\033[0m   | " + colors[23] + cursor[23] + " DetectDee")
-			fmt.Println(colors[9] + cursor[9] + " DetectDee" + "\033[0m   | " + colors[24] + cursor[24] + " DetectDee")
-			fmt.Println(colors[10] + cursor[10] + " DetectDee" + "\033[0m   | " + colors[25] + cursor[25] + " DetectDee")
-			fmt.Println(colors[11] + cursor[11] + " DetectDee" + "\033[0m   | " + colors[26] + cursor[26] + " DetectDee")
-			fmt.Println(colors[12] + cursor[12] + " DetectDee" + "\033[0m   | " + colors[27] + cursor[27] + " DetectDee")
-			fmt.Println(colors[13] + cursor[13] + " DetectDee" + "\033[0m   | " + colors[28] + cursor[28] + " DetectDee")
-			fmt.Println(colors[14] + cursor[14] + " DetectDee" + "\033[0m   | " + colors[29] + cursor[29] + " DetectDee")
-			fmt.Println("")
+			fmt.Println(" ")
+			fmt.Println(cutlass[0])
+			fmt.Println(cutlass[1] + colors[0] + cursor[0] + " All   \033[2m(58 Tools)" + "\033[0m")
+			fmt.Println(cutlass[2] + colors[1] + cursor[1] + " Osint \033[2m(Open-source Intelligence)" + "\033[0m")
+			fmt.Println(cutlass[3] + colors[2] + cursor[2] + " Exploitation" + "\033[0m")
+			fmt.Println(cutlass[4] + colors[3] + cursor[3] + " Post-Exploitation" + "\033[0m")
+			fmt.Println(cutlass[5] + colors[4] + cursor[4] + " Reporting" + "\033[0m")
+			fmt.Println(cutlass[6])
+			fmt.Println(cutlass[7])
+			fmt.Println(cutlass[8] + colors[5] + cursor[5] + " Settings" + "\033[0m")
+			fmt.Println(cutlass[9] + colors[6] + cursor[6] + " Search" + "\033[0m")
+			fmt.Println(cutlass[10] + colors[7] + cursor[7] + " Go Back" + "\033[0m")
+			fmt.Println(cutlass[11])
+			fmt.Println(" ")
+			fmt.Println("\033[2m (k = up, j = down, space = submit) \033[0m")
+			fmt.Println(" ")
+
+			/* fmt.Println(" ")
+			fmt.Println(colors[0] + cursor[0] + " Go Back" + "\033[0m")
+			fmt.Println(" ")
+			fmt.Println("  OSINT TOOLS       |  EXPLOITATION TOOLS  |  POST-EXPLOITATION TOOLS")
+			fmt.Println(" -------------------|----------------------|-------------------------")
+			fmt.Println(colors[1] + cursor[1] + " DetectDee" + "\033[0m        | " + colors[2] + cursor[2] + " SqlMap" + "\033[0m            | " + colors[3] + cursor[3] + " Reporting")
+			fmt.Println(colors[1] + cursor[1] + " WhatWeb" + "\033[0m          | " + colors[2] + cursor[2] + " Netcat" + "\033[0m            | " + colors[3] + cursor[3] + " Ghost Framework")
+			fmt.Println(colors[1] + cursor[1] + " Nmap" + "\033[0m             | " + colors[2] + cursor[2] + " Aircrack-ng" + "\033[0m       | " + colors[3] + cursor[3] + " SSH-Snake")
+			fmt.Println(colors[1] + cursor[1] + " Recon-ng" + "\033[0m         | " + colors[2] + cursor[2] + " Hashcat" + "\033[0m           | " + colors[3] + cursor[3] + " psEmpire")
+			fmt.Println(colors[1] + cursor[1] + " theHarvester" + "\033[0m     | " + colors[2] + cursor[2] + " Hydra" + "\033[0m             | " + colors[3] + cursor[3] + " Mimikatz")
+			fmt.Println(colors[1] + cursor[1] + "     " + "\033[0m             | " + colors[2] + cursor[2] + " Nikto" + "\033[0m             | " + colors[3] + cursor[3] + " LaZagne")
+			fmt.Println(" ")
+			fmt.Println(" ")
+			fmt.Println(" ")
+			fmt.Println(" ")
+			fmt.Println(" ")
+			fmt.Println(" ") */
 		}
 
 		first_loop = false
@@ -198,7 +232,7 @@ func mainMenu(init_clear bool) {
 		}
 
 		// Handle "k" Key Press (aka up)
-		if b[0] == 107 {
+		if b[0] == 107 || b[0] == 75 {
 			if hovering == 0 {
 				hovering = len(options) - 1
 			} else {
@@ -207,7 +241,7 @@ func mainMenu(init_clear bool) {
 		}
 
 		// Handle "j" Key Press (aka down)
-		if b[0] == 106 {
+		if b[0] == 106 || b[0] == 74 {
 			if hovering == len(options)-1 {
 				hovering = 0
 			} else {
@@ -232,7 +266,7 @@ func mainMenu(init_clear bool) {
 			}
 		}
 
-		if b[0] == 106 || b[0] == 107 || first_loop {
+		if b[0] == 106 || b[0] == 74 || b[0] == 107 || b[0] == 75 || first_loop {
 			for i := range options {
 				if i == hovering {
 					colors[i] = "\033[32m"
@@ -257,7 +291,7 @@ func mainMenu(init_clear bool) {
 			fmt.Println(ke + kraken[4] + "\033[0m\033[2m A pirate-themed penetration testing framework,")
 			fmt.Println(ke + kraken[5] + "\033[0m\033[2m built using reliable tools and custom scripts.")
 			fmt.Println(ke + kraken[6] + "\033[0m")
-			fmt.Println(ke + kraken[7] + "\033[0m" + colors[0] + cursor[0] + " man-o-war    \033[2m(tools)")
+			fmt.Println(ke + kraken[7] + "\033[0m" + colors[0] + cursor[0] + " armory       \033[2m(tools)")
 			fmt.Println(ke + kraken[8] + "\033[0m" + colors[1] + cursor[1] + " booty        \033[2m(captures)")
 			fmt.Println(ke + kraken[9] + "\033[0m" + colors[2] + cursor[2] + " sos          \033[2m(help)")
 			fmt.Println(ke + kraken[10] + "\033[0m" + colors[3] + cursor[3] + " bounty       \033[2m(report issue)")
